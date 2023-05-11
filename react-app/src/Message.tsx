fd4b05b7c5d0965637083540ffdfafade060a2ca
Paul Garcia <paul.ngarcia@yahoo.com>
2023-05-10 22:07:28 -0500

    just the beginning

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,10 @@
+
+//PascalCasing
+function Message(){
+    const name = 'Paul Garcia';
+    if (name)
+        return <h1>Hello {name}!</h1>;
+    return <h1>Hello world!</h1>
+}
+
+export default Message;
