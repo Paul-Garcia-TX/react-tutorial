44fb3bac1a9854e32a5ecd5e9a98d139922289c3
Paul Garcia <paul.ngarcia@yahoo.com>
2023-05-15 12:00:55 -0500

    finished yt exercises

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,25 @@
 import Alert from "./components/Alert";
 import Button from "./components/Button";
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+    const [alertVisible, setAlertVisibility] = useState(false);
+
     return (
         <div>
-            <Button color="danger" onClick={() => console.log('Clicked')}>
+            {alertVisible && (
+                <Alert onClose={() => setAlertVisibility(false)}>
+                    <strong>Alert!</strong>
+                    <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="alert"
+                        aria-label="close"
+                    ></button>
+                </Alert>
+            )}
+
+            <Button color="danger" onClick={() => setAlertVisibility(true)}>
                 My Button
             </Button>
         </div>
